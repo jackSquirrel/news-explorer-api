11c1a2f967bf93e590af85e605cc1f434cd461e9
Timur Ismagilov <ismagilov42@yandex.ru>
2020-04-09 22:16:23 +0700

    Добавлена схема пользователя

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => /[-.\w]+@[-\w]+\.[a-z]+/.test(email),
+      message: (props) => `${props.value} неверный формат email`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  }
+},
+{versionKey: false});
+
+module.exports = mongoose.model('user', userSchema);
