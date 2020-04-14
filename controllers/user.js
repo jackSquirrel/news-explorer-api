0a3290625f374f3ff301ee537e8fe146ecfbca3e
Timur Ismagilov <ismagilov42@yandex.ru>
2020-04-14 14:58:59 +0700

    Токен не отправляется пользователю в открытую

--- controllers/user.js
+++ controllers/user.js
@@ -46,7 +46,7 @@ const login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true
         })
-        .send({ token });
+        .send({ message: 'Вы успешно авторизированы!' });
     })
     .catch(next);
 };
